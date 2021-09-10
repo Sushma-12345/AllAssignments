876d5138fd702deb73f4ab6f34f2c8fce08c271a
Sushma-12345 <sushmachalla123@gmail.com>
2021-09-10 15:20:10 +0530

    commiting the asngs

--- Assignment8/src/AssignmentOnLE/Question_2.java
+++ Assignment8/src/AssignmentOnLE/Question_2.java
@@ -1,5 +1,26 @@
 package AssignmentOnLE;
+interface checking{
+	public void check(orders obj);
+}
 
-public class Question_2 {
+public class Question_2{
+
+	public static void main(String[] args) {
+		
+		orders o1 = new orders(1001,15000,"accepted");
+        orders o2 = new orders(1008,21500,"completed");
+        orders o3 = new orders(1006,7500,"accepted");
+        orders o4= new orders(1079,15000,"dispatched");
+
+        checking c1 = (orders obj)->{
+          if (obj.price>10000&&(obj.status.equalsIgnoreCase("accepted")||obj.status.equalsIgnoreCase("completed")))
+              obj.display();
+        };
+
+        c1.check(o1);
+        c1.check(o2);
+        c1.check(o3);
+        c1.check(o4);
+	}
 
 }
